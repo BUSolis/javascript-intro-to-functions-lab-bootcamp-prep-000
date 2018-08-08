003ab8b31d7a71cc8f134208c5a582acb1e50be3
Aaron Solis <aaron.solis@rocketmail.com>
2018-08-08 03:42:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,5 +3,19 @@ function shout(string) {
 }
 
 function whisper(string) {
-  return string.toLowercase()
+  return string.toLowerCase()
+}
+
+function logShout(Hello) {
+  console.log(Hello.toUpperCase())
+}
+
+function logWhisper(Quietly) {
+  console.log(Quietly.toLowerCase())
+}
+
+function sayHiToGrandma(I love you, Grandma.) {
+  var cantHear = "I can't hear you!"
+
+  if (I love you, Grandma. === I love you, Grandma.toLowerCase()) return cantHear;
 }
